7cc51daa21af550a0f1773abe3f7614c79dbcb79
Terry Lamb <terrylamb@gmail.com>
2023-04-11 14:25:03 -0400

    Add Integer and Float samples

--- variables.rb
+++ variables.rb
@@ -1,3 +1,16 @@
+# Strings
+
 email = "terrylamb@gmail.com"
 name = "Terry Lamb"
 team_name = "Dallas Mavericks"
+
+# Integers
+
+age = 34
+favorite_number = 32
+number_online = 188
+
+# Floats
+
+account_balance = 3432.00
+amount_due = 1800.00
